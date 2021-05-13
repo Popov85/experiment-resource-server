a4be9971fa73c724c9e387872926f173d93e8341
Popov85 <andrey.popov85@gmail.com>
2021-05-13 10:45:17 +0300

    Expose an endpoint for getting public key

--- src/main/java/com/experiment/resource/config/ResourceServerConfig.java
+++ src/main/java/com/experiment/resource/config/ResourceServerConfig.java
@@ -1,51 +1,11 @@
 package com.experiment.resource.config;
 
-import io.micrometer.core.instrument.util.IOUtils;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
-
-    @Value("classpath:public-key.pem")
-    private Resource publicKey;
-
-
-    @Override
-    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-        resources.tokenStore(tokenStore());
-    }
-
-    @Bean
-    public TokenStore tokenStore() {
-        return new JwtTokenStore(jwtAccessTokenConverter());
-    }
-
-    @Bean
-    public JwtAccessTokenConverter jwtAccessTokenConverter() {
-        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
-        jwtAccessTokenConverter.setVerifierKey(getPublicKeyAsString());
-        return jwtAccessTokenConverter;
-    }
-
-    private String getPublicKeyAsString() {
-        try {
-            return IOUtils.toString(publicKey.getInputStream(), StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
 }
