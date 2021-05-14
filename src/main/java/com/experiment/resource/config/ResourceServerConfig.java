545145ba0f7706d63ecb04a9478cea57bc36abca
Popov85 <andrey.popov85@gmail.com>
2021-05-14 10:56:40 +0300

    Added SSO via GITHUB

--- src/main/java/com/experiment/resource/config/ResourceServerConfig.java
+++ src/main/java/com/experiment/resource/config/ResourceServerConfig.java
@@ -1,50 +1,45 @@
 package com.experiment.resource.config;
 
-import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.util.StreamUtils;
-
-import java.nio.charset.Charset;
-import java.security.KeyFactory;
-import java.security.interfaces.RSAPublicKey;
-import java.security.spec.X509EncodedKeySpec;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 
 @Configuration
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 
-    @Value("classpath:public-key.pem")
-    private Resource key;
+    private String clientId;
+
+    private String clientSecret;
+
+    public ResourceServerConfig( @Value("${security.oauth2.github.clientId}") String clientId,
+                                 @Value("${security.oauth2.github.clientSecret}") String clientSecret) {
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.oauth2ResourceServer(
-                c -> c.jwt(
-                        j -> j.decoder(decoder())
-                )
-        );
+        http.oauth2Login();
         http.authorizeRequests().anyRequest().authenticated();
     }
 
     @Bean
-    public JwtDecoder decoder() {
-        try {
-            RSAPublicKey publicKey = (RSAPublicKey) KeyFactory.getInstance("RSA")
-                    .generatePublic(
-                            new X509EncodedKeySpec(
-                                    Base64.decodeBase64(
-                                            StreamUtils.copyToString(key.getInputStream(), Charset.forName("UTF-8")))));
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(gitHubClient());
+    }
 
-            return NimbusJwtDecoder.withPublicKey(publicKey).build();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    private ClientRegistration gitHubClient() {
+        System.out.println(clientId);
+        return CommonOAuth2Provider.GITHUB.getBuilder("github")
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .build();
     }
 
 }
--- src/main/java/com/experiment/resource/controller/HelloController.java
+++ src/main/java/com/experiment/resource/controller/HelloController.java
@@ -1,14 +1,18 @@
 package com.experiment.resource.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Slf4j
+@Controller
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello!";
+    @GetMapping("/")
+    public String welcome(Authentication authentication) {
+        log.debug("auth = {}", authentication);
+        return "welcome.html";
     }
 
 }
